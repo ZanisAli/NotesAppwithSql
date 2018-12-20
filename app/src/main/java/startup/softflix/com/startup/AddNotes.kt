a8ac118a26738724d3a70644bd5eb816ae9e96fb
Zanis Ali <zanisali999@gmail.com>
2018-12-20 10:26:17 +0100

    addNotes activity added and designed

---
+++ app/src/main/java/startup/softflix/com/startup/AddNotes.kt
@@ -0,0 +1,13 @@
+package startup.softflix.com.startup
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import startup.softflix.com.startup.R
+
+class AddNotes : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_notes)
+    }
+}
