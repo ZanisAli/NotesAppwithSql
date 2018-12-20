3942116b4cfa57b317715ed6a5bf6cfbbaafdafc
Zanis Ali <zanisali999@gmail.com>
2018-12-20 15:18:39 +0100

    sql db creation and insertion and adding notes

--- app/src/main/java/startup/softflix/com/startup/AddNotes.kt
+++ app/src/main/java/startup/softflix/com/startup/AddNotes.kt
@@ -1,12 +1,16 @@
 package startup.softflix.com.startup
 
+import android.content.ContentValues
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_add_notes.*
 import startup.softflix.com.startup.R
 
 class AddNotes : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +26,27 @@ class AddNotes : AppCompatActivity() {
         startActivity(intent)//pass this intent to Addnotes activity and send intent to that activity
         */
         //don't do like above that makes copy and will need memory so finish the activity always
-        finish()
+
+        //finish()
+
+        //instance from database
+
+        var dbManager= DbManager(this)
+
+        var values= ContentValues()
+
+        values.put("Title",etTitle.toString())
+        values.put("Description",etDes.toString())
+
+        val ID=dbManager.Insert(values)
+        if(ID>0)//means value is inserted
+        {
+            Toast.makeText(this,"note is added", Toast.LENGTH_LONG).show()
+            finish()//don't need to stay at the page if successfull
+        }
+        else{
+
+            Toast.makeText(this,"can't add note", Toast.LENGTH_LONG).show()
+        }
     }
 }
---
+++ app/src/main/java/startup/softflix/com/startup/DbManager.kt
@@ -0,0 +1,72 @@
+package startup.softflix.com.startup
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.widget.Toast
+
+/**
+ * Created by Zanis on 12/20/2018.
+ */
+
+class DbManager{
+
+    //name of database
+    val dbName="MyNotes"
+    //database table
+    val dbTable="Notes"
+    //properties of table
+    val colID="ID"
+    val colTitle="Title"
+    val colDes="Description"
+    //version of database, it is important so that when update we can track
+    val dbVersion=1
+    //CREATE TABLE IF NOT EXISTS MyNotes (ID INTEGER PRIMARY KEY,title TEXT, Description TEXT);" because we don't want to create table everytime
+    val sqlCreateTable="CREATE TABLE IF NOT EXISTS "+ dbTable +" ("+ colID +" INTEGER PRIMARY KEY,"+
+            colTitle + " TEXT, "+ colDes +" TEXT);"
+
+    //instance of sqllite database
+    var sqlDB:SQLiteDatabase?=null
+
+
+    constructor(context: Context)
+    {
+        var db=DatabaseHelperNotes(context)
+        //write the database
+        sqlDB=db.writableDatabase
+
+    }
+
+     //database helper, create database and execute commands, with inner class it will be able to access something i will do
+    inner class DatabaseHelperNotes:SQLiteOpenHelper{
+
+         var context:Context?=null
+         //CONTEXT means this activity in which we are working
+         constructor(context:Context    ):super(context, dbName, null, dbVersion){
+             this.context=context
+         }
+         //after creating database this function will work
+         override fun onCreate(p0: SQLiteDatabase?) {
+             p0!!.execSQL(sqlCreateTable)
+             Toast.makeText(this.context,"database is created", Toast.LENGTH_LONG).show()
+
+
+         }
+
+         override fun onUpgrade(p0: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+             //drop the table if exists
+             p0!!.execSQL("Drop table IF EXISTS" + dbTable )
+
+         }
+
+     }
+
+
+    //method to insert in database
+    //content value is like getting value as key value, return type is Long
+    fun Insert(values:ContentValues):Long{
+        val ID= sqlDB!!.insert(dbTable, "",values)
+        return ID//if id is 0 means i don't know otherwise means Ok
+    }
+ }
