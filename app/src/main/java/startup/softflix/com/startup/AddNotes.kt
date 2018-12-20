37c53b5a2e36b3626d6a7d963ffdac54af25e98b
Zanis Ali <zanisali999@gmail.com>
2018-12-20 11:45:20 +0100

    menu bar and intent

--- app/src/main/java/startup/softflix/com/startup/AddNotes.kt
+++ app/src/main/java/startup/softflix/com/startup/AddNotes.kt
@@ -1,7 +1,9 @@
 package startup.softflix.com.startup
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import startup.softflix.com.startup.R
 
 class AddNotes : AppCompatActivity() {
@@ -9,5 +11,17 @@ class AddNotes : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_notes)
+    }
+
+    //when button will be clicked it will close the activity
+    fun buAdd(view: View){
+
+        /*
+        //going back to main activity on button click
+        var intent= Intent(this, MainActivity::class.java)
+        startActivity(intent)//pass this intent to Addnotes activity and send intent to that activity
+        */
+        //don't do like above that makes copy and will need memory so finish the activity always
+        finish()
     }
 }
--- app/src/main/java/startup/softflix/com/startup/MainActivity.kt
+++ app/src/main/java/startup/softflix/com/startup/MainActivity.kt
@@ -1,10 +1,17 @@
 package startup.softflix.com.startup
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.SearchView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.ticket.view.*
 
@@ -25,6 +32,51 @@ class MainActivity : AppCompatActivity() {
         //lvNotes is the id to main activity list view to show tickets there so have to set adapter to that activity
         lvNotes.adapter=myNotesAdapter
 
+    }
+
+    //this method will be called automatically like oncreate(above) method when activity fire up because of override of above oncreate
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        //now have to show my menu created on main activiy so inflate that menu
+        menuInflater.inflate(R.menu.main_menu, menu)
+        //now for search view
+        val sv= menu!!.findItem(R.id.app_bar_search).actionView as SearchView
+        //search manager
+        val sm= getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        //defining search view as searchable component
+        sv.setSearchableInfo(sm.getSearchableInfo(componentName))
+        sv.setOnQueryTextListener(object:SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String): Boolean {
+                Toast.makeText(applicationContext, query, Toast.LENGTH_LONG).show()
+                //todo search database
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    //THIS FUN is for when person click add icon on main activity it should go to add notes activity
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item != null) {
+            when(item.itemId) {
+            //addNote is id for add note icon on main activity
+                R.id.addNote->{
+                    //goto add page
+                    //intent is for interprocess comm. to send and receive data from other activites
+                    //intent takes two param, "this" means this activity and addnotes to which you want to go
+                    var intent=Intent(this, AddNotes::class.java)
+                    //then have to start that activity
+                    startActivity(intent)//pass this intent to Addnotes activity and send intent to that activity
+
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     inner class MyNotesAdapter:BaseAdapter{
